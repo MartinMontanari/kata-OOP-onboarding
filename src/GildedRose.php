5292cdf8ea8c2fefcfdaa824fefed09fcf4741cd
Martin Montanari <martinmontanari3@gmail.com>
2020-09-11 16:13:19 -0300

    finish item factory and GildedRose service

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -16,8 +16,18 @@ final class GildedRose
         $this->items = $items;
     }
 
+    public function createItems(): void
+    {
+        $this->items[0] = ItemFactory::CreateItemFromType("Aged Brie", 2, 0);
+        $this->Items[1] = ItemFactory::CreateItemFromType("Elixir of the Mongose", 5, 7);
+        $this->Items[2] = ItemFactory::CreateItemFromType("Sulfuras, Hand of Ragnaros", 0, 80);
+        $this->Items[3] = ItemFactory::CreateItemFromType("Backstage passes to a TAFKAL80ETC concert", 15, 20);
+    }
+
     public function updateQuality(): void
     {
-
+        foreach ($this->items as $item) {
+            $item->CalculateSellIn().CalculateQuality();
+        }
     }
 }
